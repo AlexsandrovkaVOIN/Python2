5b612e6ff58289c9a28145bf163ec4d23aacbbfe
Alex Alexsandrov <isip_a.m.brajnikov@mpt.ru>
2023-01-27 12:00:03 +0300

    My Python 1.0.0

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+
+print("Введите год: ")
+god = int(input())
+sum = 0
+dict = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+if(god % 4 == 0):
+    sum += 1
+for x in range(1, len(dict)+1):
+    dey = dict[x]
+    i = 1
+    while dey >= i:
+        if(i >= 10):
+            stroka = str(i)
+            sum += int(stroka[0]) + int(stroka[1])
+        else:
+            sum += i
+        i += 1
+print(sum)
